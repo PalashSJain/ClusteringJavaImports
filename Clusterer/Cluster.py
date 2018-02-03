2286f1b2f9a7d432322d494ea01b3ec05d2c5e6d
PalashSJain <pxj9579@rit.edu>
2018-02-03 15:31:58 -0500

    Added check for matching org.x.xx.xxx.ABCD with ABCD library names.

--- Clusterer/Cluster.py
+++ Clusterer/Cluster.py
@@ -3,16 +3,20 @@ from Main.Utils import Utils as Utils
 
 def cluster(libraries):
     clusters = list()
+
     while len(libraries) > 0:
         cluster = list()
-        for library in reversed(libraries):
-            if libraries.index(library) == 0:
+
+        for other_library in reversed(libraries):
+            if libraries.index(other_library) == 0:
                 continue
-            if matches(libraries[0], library):
-                cluster.append(library)
-                libraries.remove(library)
+            if matches(libraries[0], other_library):
+                cluster.append(other_library)
+                libraries.remove(other_library)
+
         cluster.append(libraries.pop(0))
         clusters.append(cluster)
+
     return clusters
 
 
@@ -20,6 +24,10 @@ def matches(f, o):
     first = f.split(".")
     other = o.split(".")
     size = len(first) if len(first) < len(other) else len(other)
+
+    if size == 1:
+        return first[len(first) - 1] == other[len(other) - 1]
+
     consecutive_matches_count = 0
     for i in range(size):
         if first[i] == other[i]:
